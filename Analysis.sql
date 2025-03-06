0495d517b2f38b9304c0fd6b200d901cf28e969f
ShivanshTyagi9 <shivelite2003@gmail.com>
2025-03-06 21:50:37 +0530

    Update File

--- Analysis.sql
+++ Analysis.sql
@@ -5,7 +5,7 @@ ORDER BY levels DESC
 LIMIT 1;
 
 -- Q2. Which countries have the most Invoices?
-SELECT COUNT(invoice_id) AS invoice, billing_country AS billing_country
+SELECT COUNT(*) AS counter, billing_country
 FROM invoice
 GROUP BY billing_country
 ORDER BY billing_country DESC;
@@ -15,3 +15,18 @@ SELECT total
 FROM invoice
 ORDER BY total DESC
 LIMIT 3;
+
+
+-- Q4. Which city has the best customers? We would like to throw a promotional Music festival in the city we made the most money. Write a query that returns one city that has the highest sum of invoices totals. Return both the city name and sum of all invoice totals.
+SELECT billing_city, SUM(total) AS counter
+FROM invoice
+GROUP BY billing_city
+ORDER BY counter DESC;
+
+-- Q5. Who is the best customer? The customer who has spent the most money will be declared the best customer. Write a query that returns the person who has spent the most money.
+SELECT customer.customer_id, customer.first_name, customer.last_name, SUM(invoice.total) as spend
+FROM customer
+JOIN invoice ON customer.customer_id = invoice.customer_id
+GROUP BY customer.customer_id
+ORDER BY spend DESC
+LIMIT 1;
