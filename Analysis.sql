614a1939e287022a45cfdce437d18e1426af056b
ShivanshTyagi9 <shivelite2003@gmail.com>
2025-03-06 21:13:56 +0530

    Update File

--- Analysis.sql
+++ Analysis.sql
@@ -1,4 +1,13 @@
-SELECT COUNT(*) as c, billing_country
-from invoice
+-- Q1. Who is the senior most employee based on job title?
+SELECT * 
+FROM employee
+ORDER BY levels DESC
+LIMIT 1;
+
+-- Q2. Which countries have the most Invoices?
+SELECT COUNT(invoice_id) AS invoice, billing_country AS billing_country
+FROM invoice
 GROUP BY billing_country
-ORDER BY c DESC;
+ORDER BY billing_country DESC;
+
+-- Q3. 
