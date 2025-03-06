c0237553d7e77441276f2934041fddaceedbc281
ShivanshTyagi9 <shivelite2003@gmail.com>
2025-03-06 21:17:19 +0530

    Update File

--- Analysis.sql
+++ Analysis.sql
@@ -10,4 +10,8 @@ FROM invoice
 GROUP BY billing_country
 ORDER BY billing_country DESC;
 
--- Q3. 
+-- Q3. What are top 3 values of total invoices?
+SELECT total 
+FROM invoice
+ORDER BY total DESC
+LIMIT 3;
